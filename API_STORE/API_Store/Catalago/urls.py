9bee16803bc16829bf00efb9bcbd1bc76df075c6
“Itamar <Ikeydar@iris-eng.com>
2024-05-16 19:27:38 +0200

    controlador de catalago terminado

--- API_STORE/API_Store/Catalago/urls.py
+++ API_STORE/API_Store/Catalago/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('get/', views.get_prod, name = 'get'),
     path('add/', views.add_or_update_prod, name = 'add'),
     path('get/<int:pk>/', views.get_prod_By_Id, name = 'getById'),
-    path('stock/', views.get_stock, name = 'getStock')
+    path('stock/<int:pk>', views.update_stock, name = 'updateStock'),
+    path('isvalid/<int:pk>', views.isValid, name = 'isValid'),
+    path('isnotvalid/<int:pk>', views.isnotValid, name = 'isnotValid'),
+    path('isnotvalid/<int:pk>', views.isnotValid, name = 'isnotValid')
 ]
 
