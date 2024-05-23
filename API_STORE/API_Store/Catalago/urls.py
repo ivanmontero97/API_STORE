876a73683ebb001727c8e1a191ff011b220f3acf
“Itamar <Ikeydar@iris-eng.com>
2024-05-23 19:52:34 +0200

    merge ivan

--- API_STORE/API_Store/Catalago/urls.py
+++ API_STORE/API_Store/Catalago/urls.py
@@ -2,23 +2,6 @@ from django.urls import path
 from . import views
 
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Api Store",
-      default_version='v1',
-      description="Documentación de la API",
-      terms_of_service="https://www.tusitio.com/terms/",
-      contact=openapi.Contact(email="contact@tusitio.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
     path('get/', views.get_prod, name = 'get'),
     path('add/', views.add_or_update_prod, name = 'add'),
